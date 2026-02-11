121cb89373e0de41656204635c9d18ad2bc2460f
Morsel <misha5486@mail.ru>
2026-02-11 23:06:16 +0300

    Revert "1.1"
    This reverts commit 5195684799f7920944ffb7e0804c314efc2b0f92.

--- lib/presentation/screens/article_detail_screen.dart
+++ lib/presentation/screens/article_detail_screen.dart
@@ -11,7 +11,7 @@ class ArticleDetailScreen extends StatefulWidget {
 
   @override
   State<ArticleDetailScreen> createState() => _ArticleDetailScreenState();
-}/////
+}
 
 
 class _ArticleDetailScreenState extends State<ArticleDetailScreen> {
